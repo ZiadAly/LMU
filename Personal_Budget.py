d58b74cf7b0d2a3325da15943403d40073ea4105
Ziad <36304317+ZiadAly@users.noreply.github.com>
2021-05-03 22:44:57 +0200

    Update Personal_Budget.py

--- Personal_Budget.py
+++ Personal_Budget.py
@@ -1,46 +1,15 @@
 import json
 from datetime import datetime
 
-#this method, it adds a new transaction to the JSON file 
-#by reading the current file 
-#then adding the new transactions
-#it doesn't take any parameters
-def add_trans():
-	# ask the user to enter the description of the new transaction
-	print("Enter description")
-	desc = input()
-	# ask the user to enter the amount of the new transaction
-	print("Enter amount")
-	amount = valid_numbers()
-	# ask the user to enter the date of the new transaction
-	print("Enter date")
-	date = valid_date()
-	#transform date type from String to Date object
-	#datetime_object = datetime.strptime(date, '%Y-%m-%d')
-	#creating a new transacction as dictionary with its attributes 
-	new_trans = {"descreption" : desc, "amount" : amount, "date": date}
-	#it opens json file for reading and writing
-	with open("data.json", "r+") as message_json:
-        #it saves the previous transactions in a varaible
-		message = json.load(message_json)
-		#it adds the new transaction to the old ones
-		message.append(new_trans)
-		#it change the current file position to 0
-		message_json.seek(0)
-		#converting the message to JSON
-		json.dump(message, message_json)
-	print("Transaction has been added")
-	print("Would you like to insert another Transaction? yes/no")
-
 #this method collects the current budget
 #by reading the current file
 #then adding every amount in the transcations
 #it takes one parameter which is the number of expected transactions
-def current_budget(num_trans):
+def Current_Budget(num_trans):
 	#it opens json file for reading and writing
-	with open("data.json", "r+") as message_json:
+	with open("data.json", "r+") as Transactions_json:
 		#it saves the previous transactions in a varaible
-		message = json.load(message_json)
+		message = json.load(Transactions_json)
 		#Declating budget varaible
 		budget = 0
 		#Declaring count for the loop
@@ -55,12 +24,55 @@ def current_budget(num_trans):
         #returning the selected transactions 
 		print(message[-int(num_trans):])
 
-#this method returns selected transactions based on the requried date
-def budeget_date(m,y):
+#this method, it adds a new transaction to the JSON file 
+#by reading the current file 
+#then adding the new transactions
+#it doesn't take any parameters
+def Add_Trans():
+	# ask the user to enter the description of the new transaction
+	print("Enter description")
+	desc = input()
+	# ask the user to enter the amount of the new transaction
+	print("Enter amount")
+	amount = Valid_Numbers()
+	# ask the user to enter the date of the new transaction
+	print("Enter date")
+	date = Valid_Date()
+	#transform date type from String to Date object
+	#datetime_object = datetime.strptime(date, '%Y-%m-%d')
+	#creating a new transacction as dictionary with its attributes 
+	new_trans = {"descreption" : desc, "amount" : amount, "date": date}
 	#it opens json file for reading and writing
-	with open("data.json", "r+") as message_json:
+	with open("data.json", "r+") as Transactions_json:
+        #it saves the previous transactions in a varaible
+		message = json.load(Transactions_json)
+		#it adds the new transaction to the old ones
+		message.append(new_trans)
+		#it change the current file position to 0
+		Transactions_json.seek(0)
+		#converting the message to JSON
+		json.dump(message, Transactions_json)
+		budget = 0
+		#Declaring count for the loop
+		i = 0
+		#a loop for collecting the current budget
+		while (i < len(message)):
+			#adding the current amounts 
+			budget = budget + int(message[i]["amount"])
+			i +=1
+		#returning the current budget
+	print("Transaction has been added")
+	print("Your Current budget is " + str(budget) + "$")
+	print("Would you like to insert another Transaction? yes/no")
+
+
+
+#this method returns selected transactions based on the requried date
+def Budeget_Date(m,y):
+	#it opens json file for reading and writing
+	with open("data.json", "r+") as Transactions_json:
 		#it saves the previous transactions in a varaible
-		message = json.load(message_json)
+		message = json.load(Transactions_json)
 		#Declaring count for the loop
 		i = 0
 		#Declaring a list to add the required transactions
@@ -84,42 +96,93 @@ def budeget_date(m,y):
 	else:
 		print(out)
 
-def valid_numbers():
+#this method returns valid integer numbers
+#it takes no paramters
+def Valid_Numbers():
+	#loop stays true till the user enter a valid input
 	while True:
+		#try and except statments
 		try:
+			#Asking the user to enter an integer number
 			n = int(input())
+		#if the user enter a different value, it handles the erorr
 		except ValueError:
 			print("please, choose the prefered number")
+			#to go to the next iteration
 			continue
 		else:
+			#to break the loop if there is no errors
 			break
+	#returning the valid number
 	return n
-
-def valid_answer():
+#this method returns valid answer yes or no
+#it takes no parameters
+def Valid_Answer():
+	#loop stays true till the user enter a valid input
 	while True:
+		#asking the user to enter a yes or no
 		answer = input()
+		#checking the user input
 		if answer.lower() != "yes" and answer.lower() != "no":
 			print("please, Choose yes or no")
+		    #to go to the next iteration
 			continue
 		else:
+			#to break the loop if there is no errors
 			break
+	#returning the valid answer
 	return answer
 
-
-def valid_date():
+#this method return valid date with a certain format
+#it takes no parameters
+def Valid_Date():
+	#loop stays true till the user enter a valid input
 	while True:
 		try:
+			#asking the user to enter a yes or no
 			answer = input()
+			#Transforming the user answer to date type
 			date = datetime.strptime(answer, '%Y-%m-%d')
+		#if the user enter a different format, 
+		#it handles the erorr and print the follwoing statment
 		except ValueError:
 			print("Please, Enter the date with this format YYYY-M-D")
+			#to go to the next iteration
 			continue
-		if date.year	> datetime.today().year:
+		#the next line checks if the user input has a right year
+		if date.year > datetime.today().year:
+
 			print("Please, Enter the right year")
+			#to go to the next iteration
 			continue
 		else:
+		    #to break the loop if there is no errors
 			break
+	#returning the right date
 	return answer
+
+
+#this method returns valid positive answer
+def Valid_Pos_Number():
+	#loop stays true till the user enter a valid input
+	while True:
+		try:
+		    #asking the user to enter a yes or no
+			t = int(input())
+		#it handles the erorr and print the follwoing statment
+		except ValueError:
+			print("please, Enter the prefered number")
+			continue
+		#checking it the input above 0
+		if t < 0:
+			print("Please, Enter postive number")
+			#to go to the next iteration
+			continue
+		else:
+		    #to break the loop if there is no errors
+			break
+	#returning a valid positive number
+	return t
 
 #########################################################################################################
 
@@ -129,58 +192,95 @@ print("""please choose,
 	2 for collecting the budget, 
 	3 for Exit
 	""")
+#loop stays true till the user enter a valid input
 while True:
 	try:
+	    #asking the user to enter a valid number
 		n = int(input())
+	#it handles the erorr and print the follwoing statment
 	except ValueError:
-		print("please, choose the prefered number")
+		print("please, choose one of the options (1 or 2 or 3)")
+		#to go to the next iteration
 		continue
+	# checking the user input 
 	if n != 1 and n != 2 and n != 3:
-		print("please, choose the prefered number")
+		print("please, choose one of the options (1 or 2 or 3)")
+		#to go to the next iteration
 		continue
 	else:
+		#to break the loop if there is no errors
 		break
-###############
-#writing 
+
+#if the user input is option one
 if(int(n) == 1):
-	add_trans()
-	answer = valid_answer()
+	#Activing the Add_trans() method
+	Add_Trans()
+	#Activating the Valid_Answer() method, to get a yes or no input
+	answer = Valid_Answer()
+#loop stays true till the user enter a valid input
 	while(True):
+		#checking if the user input is yes, it activates the Add_Trans() again
 		if(answer == "yes"):			
-			add_trans()
-			answer = valid_answer()
+			Add_Trans()
+	        #Activating the Valid_Answer() method, to get a yes or no input
+			answer = Valid_Answer()
 		else:
+			#if the user input is no, it ends the program
 			print("GoodBye")
 			break
 
 ###################################
+#if the user input is option two
 if (int(n) == 2 ):
-	print("How many transactions Would you like to see?")
-	while True:
-		try:
-			t = int(input())
-		except ValueError:
-			print("please, Enter the prefered number")
-			continue
-		if t < 0:
-			print("Please, Enter postive number")
-		else:
-			break
-	current_budget(t)
-	print("would you like to select certain date? yes/no")
-	answer2 = valid_answer()
-	if ( answer2 == "yes"):
-		print("month")
-		m = input()
-		print("year")
-		y = input()
-		budeget_date(int(m),int(y))
+	print("Would you like to specify the number of viewed Transactions ? yes/no")
+	#Activating the Valid_Answer() method, to get a yes or no input
+	answer2 = Valid_Answer()
+	#checking if the user input is yes, it activates the current_budget() method
+	if(answer2 == "yes"):
+		print("How many transactions Would you like to see?")
+	    #Activating the Valid_pos_number() method, to get a positive integer number
+		t = Valid_Pos_Number()
+		#activating the current_budget() method, 
+		#passing the number of the required transactions as a parameter
+		Current_Budget(t)
 	else:
+		#activating the current_budget() method, 
+        #expecting 10 returning transactions
+		Current_Budget(10)
+	print("would you like to select certain date? yes/no")
+	#Activating the Valid_Answer() method, to get a yes or no input
+	answer3 = Valid_Answer()
+	#checking if the user input is yes, it activates the budget_date() method
+	if ( answer3 == "yes"):
+		print("month")
+	    #Activating the Valid_numbers() method, to get an integer number
+		m = Valid_Numbers()
+		print("year")
+		#Activating the Valid_numbers() method, to get an integer number
+		y = Valid_Numbers()
+		#activating the budget_date() method, 
+		#passing the required month and  year as parameters
+		Budeget_Date(m,y)
+	else:
+		#end the program
 		print("GoodBye")
 
-
-if(int(n) == 3):
+#if the user input is option three
+if (n == 3):
+	#end the program
 	print("GoodBye")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
